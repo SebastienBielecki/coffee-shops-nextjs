a5294a75525bc27270a2f96c7ce515a7473bb331
Sebastien <seb_bielecki@hotmail.com>
2023-02-15 13:52:51 -0600

    Feat: retrieve coffee shops data from Foursquare API

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,24 @@ import coffeeStoresData from "../data/coffee-stores.json"
 const inter = Inter({ subsets: ['latin'] })
 
 export async function getStaticProps(context) {
+
+  const url = 'https://api.foursquare.com/v3/places/search?query=coffee&ll=19.41177325322429%2C-99.16912362715881&limit=6';
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: process.env.FOURSQUARE_API_KEY
+    }
+  };
+
+  const response = await fetch(url, options)
+  const data = await response.json()
+  console.log(data.results);
+    
+
   return {
     props: {
-      coffeeStores: coffeeStoresData
+      coffeeStores: data.results
     },
   }
 }
@@ -42,10 +57,10 @@ export default function Home(props) {
             {props.coffeeStores.map(store =>{
               return (
                 <Card
-                  key={store.id}
+                  key={store.fsq_id}
                   name={store.name}
-                  imgUrl={store.imgUrl}
-                  href={`/coffee-store/${store.id}`}
+                  imgUrl={store.imgUrl || "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80"}
+                  href={`/coffee-store/${store.fsq_id}`}
                   className={styles.card}
                 ></Card>
               )
