9999d400ddf81af6b98db5b14cd055f6d3727b5a
Sebastien <seb_bielecki@hotmail.com>
2023-02-17 17:28:36 -0600

    Feat: coffee stores by location
    - ask user permission and retrieve permission
    - fetch coffee stores by location with Foursquare API
    - display retrieved coffee stores on landing page when clicking View Stores nearby

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import styles from '@/styles/Home.module.css'
 import Banner from '@/components/banner'
 import Card from '@/components/card'
 import { fetchCoffeeStores } from '@/lib/coffee-stores'
+import useTrackLocation from '@/hooks/use-track-location'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,8 +21,35 @@ export async function getStaticProps(context) {
 
 export default function Home(props) {
 
+  const { handleTrackLocation, latLong, locationErrorMsg, isFindingLocation } = useTrackLocation()
+
+  const [coffeeStores, setCoffeeStores] = useState("")
+  const [coffeeStoresError, setCoffeeStoresError] = useState(null)
+
+  console.log({latLong, locationErrorMsg});
+  console.log({coffeeStores});
+
+  useEffect(() => {
+    async function setCoffeeStoresByLocation() {
+      if(latLong) {
+        try {
+          const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30)
+          console.log({fetchedCoffeeStores});
+          setCoffeeStores(fetchedCoffeeStores)
+          //set coffee stores
+        } catch (error) {
+          console.log({error});
+          setCoffeeStoresError(error.message)
+        }
+      }
+    }
+    setCoffeeStoresByLocation()
+    
+  }, [latLong])
+
   const handleOnBannerBtnClick = () => {
     console.log("Hi banner button");
+    handleTrackLocation()
   }
 
   return (
@@ -31,15 +60,35 @@ export default function Home(props) {
       </Head>
       <main className={styles.main}>
         <Banner 
-          buttonText="View stores nearby"
+          buttonText={isFindingLocation ? "Locating..." : "View stores nearby"}
           handleOnClick={handleOnBannerBtnClick}
         />
+        {locationErrorMsg !== "" && <p className='errorMsg'>Something went wrong" {locationErrorMsg}</p>}
+        {coffeeStoresError && <p className='errorMsg'>Something went wrong" {coffeeStoresError}</p>}
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} alt="hero-img"/>
         </div>
-        {props.coffeeStores.length > 0 && 
-        <div>
-          <h2 className={styles.heading2}>Toronto stores</h2>
+        {(coffeeStores.length > 0) && (
+        <div className={styles.sectionWrapper}>
+          <h2 className={styles.heading2}>Stores near me</h2>
+          <div className={styles.cardLayout}>
+            {coffeeStores.map(store =>{
+              return (
+                <Card
+                  key={store.id}
+                  name={store.name}
+                  imgUrl={store.imgUrl || "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80"}
+                  href={`/coffee-store/${store.id}`}
+                  className={styles.card}
+                ></Card>
+              )
+            })}
+          </div>
+        </div>)
+        }
+        {(props.coffeeStores.length) > 0 && 
+        <div className={styles.sectionWrapper}>
+          <h2 className={styles.heading2}>Mexico City stores</h2>
           <div className={styles.cardLayout}>
             {props.coffeeStores.map(store =>{
               return (
